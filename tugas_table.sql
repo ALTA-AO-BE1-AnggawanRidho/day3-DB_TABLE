fa1999237dbbbe30dfe36239e530ee0a0b6aab04
anggawanridho <anggawanridho@gmail.com>
2023-08-03 13:33:14 +0700

    revisi relasi database

--- tugas_table.sql
+++ tugas_table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE user (
-    user_id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE users (
+    user_id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name char(50) NOT NULL,
     password char(255) NOT NULL
 );
@@ -19,7 +19,7 @@ CREATE TABLE film (
 );
 
 CREATE TABLE category (
-    category_id int NOT NULL AUTO_INCREMENT,
+    category_id int(11) NOT NULL AUTO_INCREMENT,
     category_name char(20),
     PRIMARY KEY (category_id)
 );
@@ -43,7 +43,8 @@ CREATE TABLE category_film (
     PRIMARY KEY (category_id, film_id)
 );
 
-ALTER TABLE daftar_nonton ADD FOREIGN KEY (customer_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE cast ADD FOREIGN KEY (artist_id) REFERENCES artist(artist_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE film ADD FOREIGN KEY (film_id) REFERENCES cast(film_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE category_film DROP FOREIGN KEY category_film_ibfk_1; ALTER TABLE category_film ADD FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE ON UPDATE CASCADE; ALTER TABLE category_film ADD FOREIGN KEY (film_id) REFERENCES daftar_nonton(film_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE cast ADD FOREIGN KEY (artist_id) REFERENCES artist (artist_id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE category_film ADD FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE ON UPDATE CASCADE, ADD FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE daftar_nonton ADD FOREIGN KEY (film_id) REFERENCES film (film_id) ON DELETE CASCADE ON UPDATE CASCADE, ADD FOREIGN KEY (customer_id) REFERENCES users (user_id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE film ADD  FOREIGN KEY (film_id) REFERENCES cast (film_id) ON DELETE CASCADE ON UPDATE CASCADE;
